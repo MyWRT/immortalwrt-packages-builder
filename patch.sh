033fb3a51646964641c627e33cb7662e1b4905f3
Evans Mike <etnperlong@gmail.com>
2024-05-14 14:35:17 +0100

    patch: disable golang patch

--- patch.sh
+++ patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 export PATH=/usr/local/opt/gnu-sed/libexec/gnubin:$PATH
-find . -name 'Makefile' -not -path './packages/golang/*' -exec sed  -i '/golang-package.mk/ c\include ../golang/golang-package.mk' {} \;
+# find . -name 'Makefile' -not -path './packages/golang/*' -exec sed  -i '/golang-package.mk/ c\include ../golang/golang-package.mk' {} \;
 
 if [[ $1 =~ 'SNAPSHOT'* || $1 =~ '23.05.' ]]; then
     find . -name 'Makefile' -exec sed -i '/PKG_USE_MIPS16:=0/ c\PKG_BUILD_FLAGS:=no-mips16' {} \;
